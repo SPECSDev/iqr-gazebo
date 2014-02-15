2f3fd1857dac193deb6646f92d50e9e2bc3e3e0f
armin.duff@gmail.com <armin.duff@gmail.com@753f2241-63c6-4c07-8916-4e81ad305308>
2014-02-15 22:59:40 +0000

    enabled minus points score

--- gazebo_files/world_control/score.cpp
+++ gazebo_files/world_control/score.cpp
@@ -16,28 +16,42 @@
      pointsA=0;
      pointsB=0;
      setDisplay();
-     
  }
 
 void Score::setDisplay(){
   QString str;
-  this->display(str.sprintf("%02d:%02d",pointsA,pointsB));
+  str.sprintf("%02d:%02d",pointsA,pointsB);
+  setDigitCount(str.length());
+  this->display(str);
 }
 
 void Score::reset(){
   //std::cout<<"Score::Restart"<<std::endl;
   pointsA=pointsB=0;
+  setDigitCount(5);
   setDisplay();
 }
 
-void Score::addPointA(){
+void Score::plusPointA(){
   //std::cout<<"Score::addPointA"<<std::endl;
   pointsA++;
   setDisplay();
 }
 
-void Score::addPointB(){
+void Score::plusPointB(){
   //std::cout<<"Score::addPointB"<<std::endl;
   pointsB++;
+  setDisplay();
+}
+
+void Score::minusPointA(){
+  //std::cout<<"Score::addPointA"<<std::endl;
+  pointsA--;
+  setDisplay();
+}
+
+void Score::minusPointB(){
+  //std::cout<<"Score::addPointB"<<std::endl;
+  pointsB--;
   setDisplay();
 }
