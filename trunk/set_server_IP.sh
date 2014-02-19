052d1065b24665e81f3dde07b7262236e9f16bed
armin.duff@gmail.com <armin.duff@gmail.com@753f2241-63c6-4c07-8916-4e81ad305308>
2014-02-19 21:50:08 +0000

    beauty fixes in set_server_IP.sh

--- trunk/set_server_IP.sh
+++ trunk/set_server_IP.sh
@@ -2,15 +2,19 @@
 
 if [ $# -ne 1 ];
 then 
-    echo "Please provide an ip"
-    return
+    sed -i '/GAZEBO_MASTER_URI/d' $HOME/.bashrc
+    unset GAZEBO_MASTER_URI
+else
+    ip=$1
+    sed -i '/GAZEBO_MASTER_URI/d' $HOME/.bashrc
+    echo 'export GAZEBO_MASTER_URI='$ip':11345' >> $HOME/.bashrc
+    . $HOME/.bashrc
 fi
 
-ip=$1
 
-sed -i '/GAZEBO_MASTER_URI/d' $HOME/.bashrc
-echo 'export GAZEBO_MASTER_URI='$ip':11345' >> $HOME/.bashrc
-
-. $HOME/.bashrc
-
-echo $GAZEBO_MASTER_URI
+if ! [[ $GAZEBO_MASTER_URI ]]
+then
+    echo 'GAZEBO_MASTER_URI not set'
+else
+    echo 'GAZEBO_MASTER_URI='$GAZEBO_MASTER_URI
+fi
