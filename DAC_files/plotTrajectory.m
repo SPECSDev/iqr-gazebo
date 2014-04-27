0de97e00f4e966d1adcb9d6ab445dd773c4dcbc9
riccardocsim@gmail.com <riccardocsim@gmail.com@753f2241-63c6-4c07-8916-4e81ad305308>
2014-04-27 11:23:58 +0000

    Matlab file is now a function comment with help

--- DAC_files/plotTrajectory.m
+++ DAC_files/plotTrajectory.m
@@ -1,31 +1,42 @@
+function h = plotTrajectory(filename, varargin)
+%PLOTTRAJECTORY   Creates a plot of the robot trajectories within the arena.
+%   H = PLOTTRAJECTORY(FILENAME) returns the handle of the figure and plot
+%   the drawing of the trajectories within the default arena
+% 
+%   H = PLOTTRAJECTORY(FILENAME,VARARGIN) returns the handle of the figure and plot
+%   the drawing of the trajectories within the AMBIGUOUS arena
+%
+%   Example:
+%      filename = '/home/user/Desktop/filename.dat';
+%      h = plotTrajectory(filename)
+%
 
+%   This script is provided without ant warranty
+%   $Date: 2014/04/26 $
+%   Author: SPECS - UPF - Barcelona
+skipheader = 2;
+m = importdata(filename,';',skipheader);
 
+x = m.data(:,2);
+y = m.data(:,3);
 
-skip = 2;
-m=importdata('ambiguous.dat',';',skip);
+indexT = find(diff(y)<-2);
+indexT = [indexT(1:(end-1))+40,indexT(2:end)];
 
-x=m.data(:,2);
-y=m.data(:,3);
-
-indexT=find(diff(y)<-2);
-indexT=[indexT(1:(end-1))+40,indexT(2:end)];
-
-
-
-figure
-
+h = figure();
 hold on;
 axis equal
-%arena=imread('DAC_arena.png');
-arena=imread('DAC_arena_ambiguous.png');
-arena=flipdim(arena,1);
+if (nargin>1)
+    arena = imread('DAC_arena_ambiguous.png');
+else
+    arena = imread('DAC_arena.png');
+end
+
+arena = flipdim(arena,1);
 image(arena);
 
-
-
-x=(x-7.5)/5*size(arena,2);
-y=(y-7.5)/5*size(arena,1);
-
+x = (x-7.5)/5*size(arena,2);
+y = (y-7.5)/5*size(arena,1);
 
 for ii=1:length(indexT)
 	plotIndex=indexT(ii,1):indexT(ii,2);    	
@@ -34,10 +45,4 @@ end
    
 axis off
 
-
-
-%plot(x(collisions),y(collisions),'.r');
-%plot(x(targets),y(targets),'.g');
-
-%Ncollisions = sum(diff(collisions)>0)
-%Ntargets = sum(diff(targets)>0)
+end
