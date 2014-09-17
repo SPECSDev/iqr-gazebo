98ec784f3d66d5acfed019d20845ab35d49f40f1
armin.duff@gmail.com <armin.duff@gmail.com@753f2241-63c6-4c07-8916-4e81ad305308>
2014-09-17 12:00:53 +0000

    updated remote_setup to run in seperate terminal

--- scripts/remote_setup.sh
+++ scripts/remote_setup.sh
@@ -4,6 +4,6 @@
 #for i in {1..5}
 for i in {2,16}
 do
-    ssh  profesup@10.200.188.$i bash -s < $HOME/set_up_competition.sh
+    gnome-terminal -x sh -c   "profesup@10.200.188.$i bash -s < $HOME/iqr-gazebo/scripts/set_up_competition.sh"
 
 done
