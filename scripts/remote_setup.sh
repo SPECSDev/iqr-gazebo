5c37b972f492eb1460e2843f9842fa795d5e8911
armin.duff@gmail.com <armin.duff@gmail.com@753f2241-63c6-4c07-8916-4e81ad305308>
2015-02-04 16:31:23 +0000

    fixing remote setup competition

--- scripts/remote_setup.sh
+++ scripts/remote_setup.sh
@@ -2,8 +2,8 @@
 
 
 #for i in {1..5}
-for i in {2,16}
+for i in {4,5}
 do
-    gnome-terminal -x sh -c   "profesup@10.200.188.$i bash -s < $HOME/iqr-gazebo/scripts/set_up_competition.sh"
+    gnome-terminal -x sh -c   "ssh profesup@10.200.187.$i bash -s < $HOME/iqr-gazebo/scripts/set_up_competition.sh"
 
 done
--- scripts/set_up_basic.sh
+++ scripts/set_up_basic.sh
@@ -1,11 +1,13 @@
 cd $HOME
-#svn checkout http://iqr-gazebo.googlecode.com/svn/trunk/ iqr-gazebo
 rm -rf iqr-gazebo
-wget  -r  -np  -nH --cut-dirs=1 --reject "index.htm*" http://iqr-gazebo.googlecode.com/svn/trunk/
-mv trunk iqr-gazebo
+svn checkout http://iqr-gazebo.googlecode.com/svn/trunk/ iqr-gazebo
+#rm -rf iqr-gazebo
+#wget  -r  -np  -nH --cut-dirs=1 --reject "index.htm*" http://iqr-gazebo.googlecode.com/svn/trunk/
+#mv trunk iqr-gazebo
 cd iqr-gazebo
-export GAZEBO_MODEL_PATH=$HOME/iqr-gazebo/gazebo_files/models
-export GAZEBO_PLUGIN_PATH=$HOME/iqr-gazebo/gazebo_files/plugins
+#export GAZEBO_MODEL_PATH=$HOME/iqr-gazebo/gazebo_files/models
+#export GAZEBO_PLUGIN_PATH=$HOME/iqr-gazebo/gazebo_files/plugins
 #export GAZEBO_MASTER_URI=10.200.188.1:11345
 . ./update_compile.sh
 #. ./set_server_ip.sh 10.200.188.1
+
